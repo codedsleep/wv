4b143b9d69ba7157c1de109de6a80dc06f9c59cb
codedsleep <purchases91027@pm.me>
2026-05-11 05:57:05 +0100

    chore(0.2): add crate-level lints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+#![forbid(unsafe_code)]
+#![warn(clippy::pedantic)]
+#![allow(clippy::module_name_repetitions)]
+#![allow(clippy::must_use_candidate)]
+
 fn main() {
     println!("weave starting");
 }
